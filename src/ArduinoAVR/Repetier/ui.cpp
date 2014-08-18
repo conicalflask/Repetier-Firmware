9f2c3f31947ddb78d368a60d6d1938c591a3ced7
conicalflask <gary@plumbridge.net>
2014-08-18 22:58:48 +0100

    Fixed signed-char incomparable to unsigned-char bug causing wrong char to display on LCD

--- src/ArduinoAVR/Repetier/ui.cpp
+++ src/ArduinoAVR/Repetier/ui.cpp
@@ -658,7 +658,7 @@ u8g_uint_t u8_tx = 0, u8_ty = 0;
 
 void u8PrintChar(char c)
 {
-    switch(c)
+    switch((unsigned char)c)
     {
     case 0x7E: // right arrow
         u8g_SetFont(&u8g, u8g_font_6x12_67_75);
